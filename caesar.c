1f8646cbedc82c129fe5bb27ea0b4333624659b9
Mark Jannenga <jannengm@mail.gvsu.edu>
2016-10-04 12:15:03 -0400

    Added skeleton code from project description

--- caesar.c
+++ caesar.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
 
+char * removeDuplicates(char word []);
+char * removeDuplicates(char word []);
+void initializeEncyptArray(char key[], char encrypt[]);
+void initializeDecyptArray(char encypt[], char decrypt[]);
+void processInput(FILE * inf, FILE * outf, char substitute[]);
+
 int main(int argc, char* argv[]){
   return 0;
+}
+
+// remove duplicate characters in array word and return the result string
+char * removeDuplicates(char word []){
+  return NULL;
+}
+
+// search the first num characters in array charArray for character target
+// return a non-zero integer if found, otherwise, return 0
+int targetFound(char charArray[], int num, char target){
+  return 0;
+}
+
+// initialize the encrypt array with appropriate cipher letters according
+// to the given key
+void initializeEncyptArray(char key[], char encrypt[]){
+
+}
+
+// initialize the decrypt array with appropriate substitute letters based
+// on the encrypt array
+void initializeDecyptArray(char encypt[], char decrypt[]){
+
+}
+
+// process data from the input file and write the result to the output file
+// pass the encrypt array to parameter substitute if encryption is intended
+// pass the decrypt array to parameter substitute if decryption is intended
+void processInput(FILE * inf, FILE * outf, char substitute[]){
+
 }
