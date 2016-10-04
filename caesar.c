910d90f024bab234390665dd42c636d40ee608c0
Mark Jannenga <jannengm@mail.gvsu.edu>
2016-10-04 12:57:49 -0400

    Completed removeDuplicates

--- caesar.c
+++ caesar.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+
+#define ALPHA_LEN 26
 
 char * removeDuplicates(char word []);
-char * removeDuplicates(char word []);
+int targetFound(char charArray[], int num, char target);
 void initializeEncyptArray(char key[], char encrypt[]);
 void initializeDecyptArray(char encypt[], char decrypt[]);
 void processInput(FILE * inf, FILE * outf, char substitute[]);
 
 int main(int argc, char* argv[]){
+  //Test removeDuplicates
+  // printf("Original string: %s\n", argv[1]);
+  // printf("Duplciates removed: %s\n", removeDuplicates(argv[1]));
+
   return 0;
 }
 
 // remove duplicate characters in array word and return the result string
 char * removeDuplicates(char word []){
-  return NULL;
+  static char str[ALPHA_LEN];
+  int i, j;
+
+  j = 0;
+  memset( str, '\0', ALPHA_LEN );
+
+  for( i = 0; i < strlen(word); ++i ){
+    if ( strchr(str, word[i]) == NULL ){
+      str[j] = word[i];
+      j++;
+    }
+  }
+
+  return str;
+
 }
 
 // search the first num characters in array charArray for character target
